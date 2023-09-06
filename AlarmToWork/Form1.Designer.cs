87810af40339d727db4d2c313c015401145c14f4
Doannm <doannm@vnpt-technology.vn>
2023-09-06 15:12:47 +0700

    init

---
+++ AlarmToWork/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AlarmToWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(92, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đến giờ nghỉ ngơi rồi thưa ngài";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Xác nhận";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(400, 250);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(392, 224);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(130, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 106);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(392, 224);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Setting";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(155, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "phút";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(8, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Thông báo sau";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "My SunShine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button2;
+    }
+}
+
---
+++ AlarmToWork/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmToWork
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer timer;
+        private bool isConfirmed = false;
+        private int notificationInterval = 15;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+
+            // Lấy đường dẫn thư mục gốc của ứng dụng
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Xây dựng đường dẫn tương đối tới hình ảnh
+            string relativePath = Path.Combine("Resources", "200w.gif");
+            string imagePath = Path.Combine(appDirectory, relativePath);
+            // Kiểm tra xem tập tin hình ảnh có tồn tại không
+            if (System.IO.File.Exists(imagePath))
+            {
+                // Tạo một đối tượng hình ảnh từ đường dẫn
+                Image image = Image.FromFile(imagePath);
+
+                // Đặt hình ảnh vào PictureBox
+                pictureBox1.Image = image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+
+
+            this.FormClosing += Form1_FormClosing;
+ 
+            // Xây dựng đường dẫn tương đối tới tệp âm thanh
+            string relativePathSound = Path.Combine("Resources", "PrincessVoice.wav");
+            string soundPath = Path.Combine(appDirectory, relativePathSound);
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundPath);
+            player.Play();
+            // Khởi tạo Timer và thiết lập các thuộc tính
+            timer = new System.Timers.Timer();
+            timer.Interval = notificationInterval * 60 * 1000; ; // 15 phút
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer.Stop(); // Tạm thời tắt Timer
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.FormClosing += Form1_FormClosing;
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string relativePathSound = Path.Combine("Resources", "PrincessVoice.wav");
+            string soundPath = Path.Combine(appDirectory, relativePathSound);
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundPath);
+            player.Play();
+            ShowNotification();
+            timer.Start(); // Bật lại Timer sau khi thông báo được hiển thị
+        }
+
+        private void ShowNotification()
+        {
+            // Hiển thị cửa sổ thông báo
+            isConfirmed = false;
+            this.Invoke(new Action(() =>
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Ẩn cửa sổ khi người dùng nhấn nút Xác nhận
+            isConfirmed = true;
+            this.Hide();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Ngăn người dùng đóng cửa sổ
+            if (!isConfirmed && e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Xử lý sự kiện Load Form (nếu cần)
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            int formWidth = this.Width;
+            int formHeight = this.Height;
+
+            int xPos = (screenWidth - formWidth) / 2;
+            int yPos = (screenHeight - formHeight) / 2;
+
+            this.Location = new System.Drawing.Point(xPos, yPos);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem giá trị nhập vào TextBox có hợp lệ không
+            if (int.TryParse(textBox1.Text, out int intervalMinutes) && intervalMinutes > 0)
+            {
+                // Cài đặt thời gian lặp lại mới
+                notificationInterval = intervalMinutes;
+                timer.Interval = notificationInterval * 60 * 1000; // Đổi phút thành mili giây
+                isConfirmed = true;
+                this.Hide();
+                tabControl1.SelectTab(tabPage1);
+            }
+        }
+    }
+}
